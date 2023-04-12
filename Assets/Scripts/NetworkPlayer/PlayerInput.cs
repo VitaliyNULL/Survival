543eac6200144c9213268c68c795b4843f7700e6
Vitaliy <botmile@gmail.com>
2023-04-12 14:06:52 +0300

    fixed host migration

--- Assets/Scripts/NetworkPlayer/PlayerInput.cs
+++ Assets/Scripts/NetworkPlayer/PlayerInput.cs
@@ -74,39 +74,7 @@ namespace VitaliyNULL.NetworkPlayer
             Debug.Log("Pointer up");
             _touchedJoystick = false;
         }
-
-        private void HostMigrationResume(NetworkRunner runner)
-        {
-            Debug.Log("HostMigrationResume");
-            var joysticks = FindObjectsOfType<VariableJoystick>();
-            foreach (var joystick in joysticks)
-            {
-                if (joystick.name == "MoveJoystick")
-                {
-                    movementJoystick = joystick;
-                    break;
-                }
-
-                if (joystick.name == "WeaponJoystick")
-                {
-                    weaponJoystick = joystick;
-                    var trigger = weaponJoystick.GetComponent<EventTrigger>();
-                    EventTrigger.Entry entry1 = new EventTrigger.Entry();
-                    EventTrigger.Entry entry2 = new EventTrigger.Entry();
-
-                    trigger.triggers.Clear();
-                    entry1.eventID = EventTriggerType.PointerDown;
-                    entry1.callback.AddListener((arg0 => { EventPointerDown(); }));
-                    entry2.eventID = EventTriggerType.PointerUp;
-                    entry2.callback.AddListener((arg0 => { EventPointerUp(); }));
-                    trigger.triggers.Add(entry1);
-                    trigger.triggers.Add(entry2);
-                }
-            }
-
-            runner.AddCallbacks(this);
-        }
-
+        
         #endregion
 
         #region INetworkRunnerCallbacks
@@ -175,34 +143,35 @@ namespace VitaliyNULL.NetworkPlayer
             Debug.Log("OnHostMigration");
 
             await runner.Shutdown(shutdownReason: ShutdownReason.HostMigration);
+            SceneManager.LoadScene(0);
             // Step 2.2
             // Create a new Runner.
-            var newRunner = Instantiate(new GameObject("FusionManager").AddComponent<NetworkRunner>());
-            FusionManager fusionManager = newRunner.gameObject.AddComponent<FusionManager>();
-            fusionManager.playerController = Resources.Load<NetworkObject>("PlayerController");
-            Debug.Log(fusionManager.playerController);
-            // setup the new runner...
-            // Start the new Runner using the "HostMigrationToken" and pass a callback ref in "HostMigrationResume".
-            StartGameResult result = await newRunner.StartGame(new StartGameArgs()
-            {
-                // SessionName = SessionName,              // ignored, peer never disconnects from the Photon Cloud
-                // GameMode = gameMode,                    // ignored, Game Mode comes with the HostMigrationToken
-                HostMigrationToken = hostMigrationToken, // contains all necessary info to restart the Runner
-                HostMigrationResume = HostMigrationResume, // this will be invoked to resume the simulation
-                PlayerCount = 2,
-                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
-                // other args
-            });
-
-            // Check StartGameResult as usual
-            if (result.Ok == false)
-            {
-                Debug.LogWarning(result.ShutdownReason);
-            }
-            else
-            {
-                Debug.Log("Done");
-            }
+            // var newRunner = Instantiate(new GameObject("FusionManager").AddComponent<NetworkRunner>());
+            // FusionManager fusionManager = newRunner.gameObject.AddComponent<FusionManager>();
+            // fusionManager.playerController = Resources.Load<NetworkObject>("PlayerController");
+            // Debug.Log(fusionManager.playerController);
+            // // setup the new runner...
+            // // Start the new Runner using the "HostMigrationToken" and pass a callback ref in "HostMigrationResume".
+            // StartGameResult result = await newRunner.StartGame(new StartGameArgs()
+            // {
+            //     // SessionName = SessionName,              // ignored, peer never disconnects from the Photon Cloud
+            //     // GameMode = gameMode,                    // ignored, Game Mode comes with the HostMigrationToken
+            //     HostMigrationToken = hostMigrationToken, // contains all necessary info to restart the Runner
+            //     HostMigrationResume = HostMigrationResume, // this will be invoked to resume the simulation
+            //     PlayerCount = 2,
+            //     SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            //     // other args
+            // });
+            //
+            // // Check StartGameResult as usual
+            // if (result.Ok == false)
+            // {
+            //     Debug.LogWarning(result.ShutdownReason);
+            // }
+            // else
+            // {
+            //     Debug.Log("Done");
+            // }
         }
 
         public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
